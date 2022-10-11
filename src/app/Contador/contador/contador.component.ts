120d0359535968faaa386c96be800120fe92dc78
Harold Muñoz <harold2458@gmail.com>
2022-10-10 20:12:24 -0500

    primer commit

---
+++ src/app/Contador/contador/contador.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-contador',
+    template: `
+        <h1>{{titulo}}</h1>
+        <h3>La base es: <strong>{{base}}</strong></h3>
+        <button (click)="Acumular(+this.base)">+{{base}}</button>
+        <span> {{numero}}</span>
+        <button (click)="Acumular(-this.base)">-{{base}}</button>
+        `
+})
+export class ContadorComponent {
+    titulo: string = 'Contador app';
+    numero: number = 10;
+    base: number = 5;
+
+
+    Acumular(valor: number){
+        this.numero += valor
+    }
+}
---
+++ src/app/Heroes/Heroe/heroe.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+export class HeroeComponent{
+    nombre: string = 'Ironman'
+    edad: number = 30
+    
+    get nombreCapitalizado(){
+        return this.nombre.toUpperCase()
+    }
+
+    ObtenerNombre(): string{
+        return `${this.nombre} - ${this.edad} `
+    }
+
+    CambiarNombre(): void{
+        this.nombre= 'Spiderman'
+    }
+
+    CambiarEdad(): void{
+        this.edad = 20
+    }
+}
